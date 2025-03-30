350a4bb5d914f6f4d1f3d1545e56e5dc7dd01558
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-03-30 21:01:01 +0530

    C++ introduction
    Datattypes
    If Else
    Ternary operator

---
+++ C++Project/1.Introduction/BasicDatatypes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdint>
+#include <ostream>
+
+
+int main()
+{
+
+    //int32_t are singed 16bit integer
+    //uint32_t are Unsinged 16bit integer
+
+
+    std::int16_t i16 = 0;
+    std::uint16_t iu16 = 0U;
+
+
+    std::int32_t i32 = 0;
+    std::uint32_t iu32 = 0U;
+
+
+    std::int64_t i64 = 0;
+    std::uint64_t iu64 = 0;
+
+    float f1 = 12.0F;
+    double d1 = 12.0;
+
+    bool b1 = false;
+    bool b2 = true;
+
+    char a = 'A'; //8-bit charecter in single quotes
+
+    return 0;
+}
